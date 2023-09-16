8a350dd3a6fc8c0593bdb0f8c08a334bfd0681ed
anviaan <andre.carbajal.vargas@gmail.com>
2023-09-16 15:46:26 -0500

    remove

--- src/client/java/net/anvian/visualbookshelves/BetterBookshelvesClient.java
+++ src/client/java/net/anvian/visualbookshelves/BetterBookshelvesClient.java
@@ -2,14 +2,9 @@ package net.anvian.visualbookshelves;
 
 import net.anvian.visualbookshelves.config.client.ConfigManager;
 import net.anvian.visualbookshelves.networking.client.Networking;
-import net.anvian.visualbookshelves.render.BetterBookshelvesModelProvider;
-import net.anvian.visualbookshelves.render.ChiseledBookshelfBlockEntityRenderer;
 import net.anvian.visualbookshelves.render.InGameHudBookPreview;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,8 +16,6 @@ public class BetterBookshelvesClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		Networking.init();
 
-		ModelLoadingRegistry.INSTANCE.registerResourceProvider(manager -> new BetterBookshelvesModelProvider());
-		BlockEntityRendererFactories.register(BlockEntityType.CHISELED_BOOKSHELF, context -> new ChiseledBookshelfBlockEntityRenderer());
 		HudRenderCallback.EVENT.register(InGameHudBookPreview::renderCrosshair);
 
 		ConfigManager.loadConfig();
